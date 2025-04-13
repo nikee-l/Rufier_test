88e26b3ccb45657f4558da10597d405ca6ae498e
nix <test@email.com>
2025-04-13 15:42:57 +0300

    finish win1

--- src/my.app.py
+++ src/my.app.py
@@ -1,24 +1,35 @@
 from inst import*
+from PyQt5.QtWidgets import*
+from second_win import * 
 
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
-        self.set.appear()
+        self.set_appear()
         self.initUI()
         self.connects()
         self.show()
     
     def set_appear(self):
         self.setWindowTitle(txt_title)
-        self.relize(win_width, win_height)
+        self.resize(win_width, win_height)
         self.move(win_x, win_y)
     def initUI(self):
-        self.hello_text = QLabel(txt_Hello)
+        self.hello_text = QLabel(txt_hello)
         self.instruction = QLabel(txt_instruction)
-        self.button = QPushButton(txt_next)
+        self.btn_next = QPushButton(txt_next)
         self.layout = QVBoxLayout()
-        self.hello_text.addWidget(self.layout)
-        self.instruction.addWidget(self.layout)
-        self.button.addWidget(self.layout)
+        self.layout.addWidget(self.hello_text)
+        self.layout.addWidget(self.instruction)
+        self.layout.addWidget(self.btn_next)
+        self.setLayout(self.layout)
+
     def connects(self):
-        pass
+        self.btn_next.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+        self.tw = TestWin()
+
+app = QApplication([])
+mw = MainWin()
+app.exec_()
